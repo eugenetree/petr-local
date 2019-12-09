25cbb55bc6b9bcbbe2288d05adee147322763470
oziged <oziged@gmail.com>
2019-12-09 15:59:19 +0200

    move grid from section to separate component

---
+++ components/Grid.vue
@@ -0,0 +1,165 @@
+<template>
+        <div class="grid">
+          <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+         <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+           <div class="grid-item overlay">
+            <div class="bg-img">
+              <img src="/home/popular-1.png" alt="">
+            </div>
+            <span class="top">Safety in</span>
+            <span class="bottom">Costa Rica</span>
+          </div>
+        </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+      .grid {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        .grid-item {
+          cursor: pointer;
+          position: relative;
+          height: 340px;
+          width: calc((100% - 75px) / 4);
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          margin-right: 25px;
+          margin-bottom: 25px;
+          background-color: black;
+          transition: .3s;
+          &:nth-child(4n) {
+            margin-right: 0;
+          }
+          &::after {
+            background-color: rgba(0, 0, 0, .2);
+          }
+          .bg-img {
+            position: absolute;
+            z-index: 0;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+          }
+          &:hover {
+            .bottom {
+              border-bottom: 1px solid white;
+            }
+            &::after {
+              background-color: rgba(0, 0, 0, .5);
+            }
+          }
+          span {
+            color: $white;
+            position: relative;
+            z-index: 1;
+          }
+          .top {
+            font-size: 1.25em;
+          }
+          .bottom {
+            font-size: 1.5em;
+            font-weight: bold;
+            letter-spacing: .9px;
+            border-bottom: 1px solid transparent;
+            transition: .3s;
+          }
+        }
+      }
+
+  @media (max-width: 1150px) {
+    .grid {
+      .grid-item {
+        width: calc((100% - 50px) / 3);
+        &:nth-child(4n) {
+          margin-right: 25px;
+        }
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+
+    @media (max-width: 800px) {
+    .grid {
+      .grid-item {
+        width: calc((100% - 25px) / 2);
+        &:nth-child(3n) {
+          margin-right: 25px;
+        }
+        &:nth-child(2n) {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 800px) {
+    .grid {
+      .grid-item {
+        margin-right: 0;
+        width: 100%;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -21,64 +21,7 @@
     <section class="popular-section">
       <div class="content">
         <h3>Popular searches</h3>
-        <div class="grid">
-          <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-         <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-           <div class="grid-item overlay">
-            <div class="bg-img">
-              <img src="/home/popular-1.png" alt="">
-            </div>
-            <span class="top">Safety in</span>
-            <span class="bottom">Costa Rica</span>
-          </div>
-        </div>
+        <grid/>
       </div>
     </section>
   </div>
@@ -86,10 +29,11 @@
 
 <script>
 import SearchBox from '@/components/SearchBox.vue'
-
+import Grid from '@/components/Grid.vue'
 export default {
   components: {
-    SearchBox
+    SearchBox,
+    Grid
   }
 }
 </script>
@@ -137,62 +81,6 @@ export default {
         font-size: 1.6em;
         margin-bottom: 35px;
       }
-      .grid {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        .grid-item {
-          cursor: pointer;
-          position: relative;
-          height: 340px;
-          width: calc((100% - 75px) / 4);
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin-right: 25px;
-          margin-bottom: 25px;
-          background-color: black;
-          transition: .3s;
-          &:nth-child(4n) {
-            margin-right: 0;
-          }
-          &::after {
-            background-color: rgba(0, 0, 0, .2);
-          }
-          .bg-img {
-            position: absolute;
-            z-index: 0;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-          }
-          &:hover {
-            .bottom {
-              border-bottom: 1px solid white;
-            }
-            &::after {
-              background-color: rgba(0, 0, 0, .5);
-            }
-          }
-          span {
-            color: $white;
-            position: relative;
-            z-index: 1;
-          }
-          .top {
-            font-size: 1.25em;
-          }
-          .bottom {
-            font-size: 1.5em;
-            font-weight: bold;
-            letter-spacing: .9px;
-            border-bottom: 1px solid transparent;
-            transition: .3s;
-          }
-        }
-      }
     }
   }
 
@@ -202,42 +90,10 @@ export default {
     }
   }
 
-  @media (max-width: 1150px) {
-    .popular-section .content .grid {
-      .grid-item {
-        width: calc((100% - 50px) / 3);
-        &:nth-child(4n) {
-          margin-right: 25px;
-        }
-        &:nth-child(3n) {
-          margin-right: 0;
-        }
-      }
-    }
-  }
-
   @media (max-width: 800px) {
-    .popular-section .content .grid {
-      .grid-item {
-        width: calc((100% - 25px) / 2);
-        &:nth-child(3n) {
-          margin-right: 25px;
-        }
-        &:nth-child(2n) {
-          margin-right: 0;
-        }
-      }
-    }
-  }
-
-  @media (max-width: 800px) {
-    .popular-section .content .grid {
-      .grid-item {
-        margin-right: 0;
-        width: 100%;
-        &:nth-child(3n) {
-          margin-right: 0;
-        }
+    .popular-section .content{
+      h3 {
+        margin-bottom: 15px;
       }
     }
   }
